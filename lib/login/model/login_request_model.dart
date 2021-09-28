60f2480dceec5da64cc8596b4d6ebd305a9fd489
Suat Özkaya <suatozkaya@gmail.com>
2021-09-28 21:56:31 +0300

    Create login_request_model.dart

---
+++ lib/login/model/login_request_model.dart
@@ -0,0 +1,18 @@
+class LoginRequestModel {
+  String? email;
+  String? password;
+
+  LoginRequestModel({this.email, this.password});
+
+  LoginRequestModel.fromJson(Map<String, dynamic> json) {
+    email = json['email'];
+    password = json['password'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['email'] = this.email;
+    data['password'] = this.password;
+    return data;
+  }
+}
