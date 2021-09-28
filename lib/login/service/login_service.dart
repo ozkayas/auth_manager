f8216da7961bb8ce6c85388dc347fb5a0c2c60bc
Suat Özkaya <suatozkaya@gmail.com>
2021-09-28 22:19:15 +0300

    Update login_service.dart

--- lib/login/service/login_service.dart
+++ lib/login/service/login_service.dart
@@ -1,3 +1,19 @@
-class LoginService {
-  final String baseUrl = 'https://reqres.in/api/login';
+import 'package:auth_manager/login/model/login_request_model.dart';
+import 'package:auth_manager/login/model/login_response_model.dart';
+import 'package:get/get_connect.dart';
+import 'package:get/get_connect/http/src/status/http_status.dart';
+
+class LoginService extends GetConnect {
+  final String loginUrl = 'https://reqres.in/api/login';
+  //final String path = '/api/login';
+
+  Future<LoginResponseModel?> fetchLogin(LoginRequestModel model) async {
+    final response = await post(baseUrl, model.toJson());
+
+    if (response.statusCode == HttpStatus.ok) {
+      return LoginResponseModel.fromJson(response.body);
+    } else {
+      return null;
+    }
+  }
 }
