ce19132eea1331edd5f2dc2b957c233775664706
Suat Özkaya <suatozkaya@gmail.com>
2021-09-28 22:52:55 +0300

    Create home_view.dart

---
+++ lib/home_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('HOME VIEW'),
+      ),
+    );
+  }
+}
