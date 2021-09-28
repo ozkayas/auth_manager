7112b078c98a68647e68d71687b96a0a0bf592b6
Suat Özkaya <suatozkaya@gmail.com>
2021-09-28 08:44:58 +0300

    Add TextFormFields

--- lib/login/login_view.dart
+++ lib/login/login_view.dart
@@ -12,9 +12,48 @@ class _LoginViewState extends State<LoginView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Form(
-        child: Column(children: [TextFormField(), TextFormField()]),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+        child: Form(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            TextFormField(
+              decoration: inputDecoration('E-mail', Icons.person),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            TextFormField(
+              decoration: inputDecoration('Password', Icons.lock),
+            )
+          ]),
+        ),
       ),
     );
   }
+}
+
+InputDecoration inputDecoration(String labelText, IconData iconData,
+    {String? prefix, String? helperText}) {
+  return InputDecoration(
+    contentPadding: EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+    helperText: helperText,
+    labelText: labelText,
+    labelStyle: TextStyle(color: Colors.grey),
+    fillColor: Colors.grey.shade200,
+    //fillColor: Constant.kTextFielColor,
+    //hintText: labelText,
+    filled: true,
+    prefixText: prefix,
+    prefixIcon: Icon(
+      iconData,
+      size: 20,
+    ),
+    prefixIconConstraints: BoxConstraints(minWidth: 60),
+    enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(30),
+        borderSide: BorderSide(color: Colors.black)),
+    focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(30),
+        borderSide: BorderSide(color: Colors.black)),
+  );
 }
