46b03998e4cf60c4c1898f8411eecbc697a2db47
Suat Özkaya <suatozkaya@gmail.com>
2021-09-28 07:53:21 +0300

    Create login view

---
+++ lib/login/login_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({Key? key}) : super(key: key);
+
+  @override
+  _LoginViewState createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Form(
+        child: Column(children: [TextFormField(), TextFormField()]),
+      ),
+    );
+  }
+}
