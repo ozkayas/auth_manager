b65bffbe5de75e1adffc6377131c6bc8bf12ebcd
Suat Özkaya <suatozkaya@gmail.com>
2021-10-02 12:50:59 +0300

    Refactor views

--- lib/splash_view.dart
+++ lib/splash_view.dart
@@ -21,28 +21,35 @@ class SplashView extends StatelessWidget {
       future: initializeSettings(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return Scaffold(
-              body: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: CircularProgressIndicator(),
-                ),
-                Text('Loading...'),
-              ],
-            ),
-          ));
+          return waitingView();
         } else {
           if (snapshot.hasError)
-            return Scaffold(
-                body: Center(child: Text('Error: ${snapshot.error}')));
+            return errorView(snapshot);
           else
             return OnBoard();
         }
       },
     );
+  }
+
+  Scaffold errorView(AsyncSnapshot<Object?> snapshot) {
+    return Scaffold(body: Center(child: Text('Error: ${snapshot.error}')));
+  }
+
+  Scaffold waitingView() {
+    return Scaffold(
+        body: Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: CircularProgressIndicator(),
+          ),
+          Text('Loading...'),
+        ],
+      ),
+    ));
   }
 }
